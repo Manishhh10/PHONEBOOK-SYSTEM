3914f0fd1c019b0aa1128d6e89cd27baddd8597c
manishhh10 <manishbhatt2121714@gmail.com>
2024-02-02 14:53:25 +0545

    intital commit

---
+++ login.py
@@ -0,0 +1,33 @@
+from tkinter import*
+win=Tk()
+win.geometry("500x300")
+
+
+def add():
+    win.destroy()
+    import register
+
+btn=Button(text="Register Now",command=add)
+btn.place(x=210,y=165)
+
+def add():
+    a=e1.get()
+    lbl.config(text=a)
+    e1.delete(0,END)
+    a=e2.get()
+    lbl.config(text=a)
+    e2.delete(0,END)
+name=Label(text='ID')
+name.pack(padx=0,pady=0)
+e1=Entry()
+e1.pack(pady=10)
+name=Label(text='Password')
+name.pack(pady=0) 
+e2=Entry()
+e2.pack(pady=10)
+lbl=Label(text='')
+btn=Button(text="Log-In",command=add)
+btn.pack()
+
+
+win.mainloop()
---
+++ register.py
@@ -0,0 +1,40 @@
+from tkinter import *
+root = Tk()
+root.geometry("500x500")
+
+
+name_label = Label(text="First Name")
+name_label.pack()
+name_entry = Entry()
+name_entry.pack()
+
+last_name_label = Label(text="Last Name")
+last_name_label.pack()
+last_name_entry = Entry()
+last_name_entry.pack()
+
+
+gender_label= Label(text="Email")
+gender_label.pack()
+gender_entry=Entry()
+gender_entry.pack()
+
+
+age_label=Label(text="Password")
+age_label.pack()
+age_entry = Entry()
+age_entry.pack()
+
+output = Label(text="")
+output.pack()
+
+btn=Button(text="Save")
+btn.pack()
+
+def add():
+    root.destroy()
+    import login
+btn=Button(text="Go To Login Page!",command=add)
+btn.pack(pady=10)
+
+root.mainloop()
