21623b1a6a3fff4d5915feff8377a664a0a6bbf9
PrajwalChaulagain <prajwalchaulagai098@gmail.com>
2024-02-03 18:53:57 +0545

    register design completed

--- register.py
+++ register.py
@@ -1,40 +1,51 @@
 from tkinter import *
 root = Tk()
-root.geometry("500x500")
+root.geometry("698x520")
+root.resizable(0,0)
+
+# BACKROUND COLOUR
+root.config(bg="#333333")
+
+# CREATING WIDGETS AND ADDING COLORS
+
+register_label=Label(root,text="REGISTER",font=("Arial",22),bg="#333333",fg="White")
+email_label=Label(text='EMAIL',font=("Adiro",12),bg="#333333",fg="White",pady=10)
+username_label=Label(text='USERNAME',font=("Adiro",12),bg="#333333",fg="White",pady=12)
+password_label=Label(text='PASSWORD',font=("Adiro",12),bg="#333333",fg="White",pady=1)
+and_label=Label(root,text=" OR ",font=("Arial",11),bg="#333333",fg="White",pady=10)
+
+# ADDING BOX 
+
+email_entry=Entry(root,font=("Adior",15))
+email_entry.pack(pady=10)
+username_entry=Entry(root,font=("Adior",15))
+password_entry=Entry(root,font=("Adior",15))
+
+# PLACING THE WIDGETS
+
+email_label.place(x=320,y=82)
+username_label.place(x=305,y=152)
+register_label.pack(side=TOP,pady=20)
+password_label.place(x=305,y=235)
+email_entry.place(x=240,y=115,height=40,width=225)
+username_entry.place(x=240,y=187,height=40,width=225)
+password_entry.place(x=240,y=260,height=40,width=225)
+and_label.place(x=335,y=365)
+
+# ADDING BUTTTONS
+save_btn=Button(root,text="SAVE",font=("Arial Bold",10),bg="White",fg=("#2148C0"),width=27,height=2)
+save_btn.place(x=242,y=330)
+back_btn=Button(root,text="GO TO LOGIN PAGE !",font=("Arial Bold",10),bg="White",fg=("#2148C0"),width=27,height=2)
+back_btn.place(x=242,y=398)
 
 
-name_label = Label(text="First Name")
-name_label.pack()
-name_entry = Entry()
-name_entry.pack()
-
-last_name_label = Label(text="Last Name")
-last_name_label.pack()
-last_name_entry = Entry()
-last_name_entry.pack()
 
 
-gender_label= Label(text="Email")
-gender_label.pack()
-gender_entry=Entry()
-gender_entry.pack()
 
-
-age_label=Label(text="Password")
-age_label.pack()
-age_entry = Entry()
-age_entry.pack()
-
-output = Label(text="")
-output.pack()
-
-btn=Button(text="Save")
-btn.pack()
-
-def add():
+# def add():
     
-    import login
-btn=Button(text="Go To Login Page!",command=add)
-btn.pack(pady=10)
+#     import login
+# btn=Button(text="Go To Login Page!",command=add)
+# btn.pack(pady=10)
 
 root.mainloop()
